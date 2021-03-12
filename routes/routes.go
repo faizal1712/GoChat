67e152e8b7e62380ae6d137ba5067bdfc981d84a
faizal1712 <76579749+faizal1712@users.noreply.github.com>
2021-03-12 16:02:37 +0530

    Updated routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -3,9 +3,9 @@ package routes
 import (
 	"contact/socket"
 	"fmt"
-	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -17,11 +17,18 @@ func CreateRouter() {
 }
 
 func InititalizeRoutes() {
+
+	router.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
+	})
 	router.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./asset"))))
 	router.Handle("/socket.io/", socket.Server)
 }
 
 func StartServer() {
 	fmt.Println("Server Started at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	// log.Fatal(http.ListenAndServe(":8080", router))
+	http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Access-Control-Allow-Origin", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(router))
 }
